6a08430f9c5a152fe858bc0d1b64b0b315813b66
Erik Beltran <erikb0696@gmail.com>
2024-09-30 21:25:38 -0500

    favicon changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,12 @@ import "./globals.css";
 import NextTopLoader from "nextjs-toploader";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster";
+import favicon from "../public/favicon.ico";
 
 const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Admin Cars",
+  title: "Rental Cars",
   description: "Rental Cars by Erik B",
 };
 
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <link rel="shortcut icon" href={favicon.src} type="image/x-icon" />
         <body className={outfit.className}>
           <NextTopLoader color="#000" />
           {children}
